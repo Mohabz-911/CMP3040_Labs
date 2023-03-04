87d948c26a351d4285dbc0f99ded6e6bb042dab9
Mohab <hoobamz@gmail.com>
2023-03-04 19:05:18 +0200

    Using avr toolchain

--- main.c
+++ main.c
@@ -1,15 +1,31 @@
 #define __AVR_ATmega328P__
 #include <avr/io.h>
-#include <util/delay.h>
+#include <avr/interrupt.h>
 #define 	SET_BIT(REG_NAME,BIT_NUMBER) 		REG_NAME |= (1<<BIT_NUMBER)
 #define 	CLEAR_BIT(REG_NAME,BIT_NUMBER) 		REG_NAME &= (~(1<<BIT_NUMBER))
 #define 	TOGGLE_BIT(REG_NAME,BIT_NUMBER) 	REG_NAME ^= (1<<BIT_NUMBER)
 
+ISR(INT0_vect)
+{
+    TOGGLE_BIT(PORTC,0);
+}
+
+
+void INT0_Init(void)
+{
+    CLEAR_BIT(SREG,0);
+    CLEAR_BIT(DDRD,2);
+    SET_BIT(EIMSK,0);
+    EICRA |= 0x03;      //rising edge
+    SET_BIT(SREG,0);
+}
 
 void main(void)
 {
     //Setup
-
+    INT0_Init();
+    SET_BIT(DDRC,0);
+    SET_BIT(PORTC,0);
     while(1)
     {
         
